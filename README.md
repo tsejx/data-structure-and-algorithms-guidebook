6e9561447004addf868b5bc292d85501127672ec
tsejx <tsejx@foxmail.com>
2020-06-17 23:41:58 +0800

    :rocket: chore: project code

---
+++ README.md
@@ -0,0 +1,12 @@
+#<div align="center">
+<img width="200" src="http://img.mrsingsing.com/data-structure-and-algorithms-icon.png">
+
+</div>
+
+<h1 align="center">Data Structure And Algorithms Guidebook</h1>
+
+<p align="center">📚 数据结构与算法知识图谱 关于数据结构、算法、手写代码、编程能力提升</p>
+
+<div align="center"><a href="https://tsejx.github.io/data-structure-and-algorithms-guidebook/" target="_blank">立即开始</a></div>
+
+</div>
---
+++ config/config.js
@@ -0,0 +1,18 @@
+export default {
+  mode: 'site',
+  hash: true,
+  title: 'Data Structure and Algorithms Guidebook',
+  description: '数据结构与算法完全知识体系',
+  base: '/data-structure-and-algorithms-guidebook/',
+  publicPath: '/data-structure-and-algorithms-guidebook/',
+  favicon: './favicon.ico',
+  logo: 'http://img.mrsingsing.com/data-structure-and-algorithms-favicon.svg',
+  exportStatic: {},
+  navs: [
+    null,
+    {
+      title: 'Github',
+      path: 'https://github.com/tsejx/data-structure-and-algorithms-guidebook',
+    },
+  ],
+};
